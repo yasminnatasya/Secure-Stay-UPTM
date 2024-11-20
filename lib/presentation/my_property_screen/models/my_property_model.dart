5aecdeaee13acb446e4cf450d0de011535a79637
yasminnatasya <yasminnatasya29@gmail.com>
2024-11-20 18:39:41 +0800

    new update

--- lib/presentation/my_property_screen/models/my_property_model.dart
+++ lib/presentation/my_property_screen/models/my_property_model.dart
@@ -1,6 +1,6 @@
 import '../../../core/app_export.dart';
 import 'bedroomlist_item_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
 /// This class defines the variables used in the [my_property_screen],
 /// and is typically used to hold data that is passed between different parts of the application.
@@ -8,18 +8,5 @@ class MyPropertyModel {
   Rx<List<BedroomlistItemModel>> bedroomlistItemList =
       Rx<List<BedroomlistItemModel>>([]);
 
-  MyPropertyModel() {
-    fetchProperties();
-  }
-
-  void fetchProperties() async {
-    FirebaseFirestore.instance
-        .collection('accommodations')
-        .snapshots()
-        .listen((snapshot) {
-      bedroomlistItemList.value = snapshot.docs
-          .map((doc) => BedroomlistItemModel.fromFirestore(doc))
-          .toList();
-    });
-  }
+  MyPropertyModel(); // Empty constructor
 }
